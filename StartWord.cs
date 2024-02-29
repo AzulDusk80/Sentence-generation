e1b10b4f6a95630a5b43111d01ef844ca2418660
Jesus Olvera-Lara <51460948+AzulDusk80@users.noreply.github.com>
2024-02-29 09:37:44 -0600

    Update StartWord.cs

--- StartWord.cs
+++ StartWord.cs
@@ -1,1 +1,21 @@
-﻿
+﻿public class MiddleWord : Word
+{
+	public MiddleWord(string name, string after, string before = " ") : base(name, after, before)
+	{
+		
+	}
+
+    public override void addBefores(string word)
+	{
+        befores.Add(word);
+    }
+
+    public override void addAfters(string word)
+    {
+        if (string.IsNullOrEmpty(word))
+        {
+            throw new ArgumentNullException(word);
+        }
+        afters.Add(word);
+    }
+}
